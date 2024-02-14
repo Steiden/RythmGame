f6972a40627ff566fe36944534586ba5c8cd2c96
Steiden <baldindaniilza@mail.ru>
2024-02-14 15:02:37 +0500

    Create class and activity of creating new level of song

---
+++ app/src/main/java/com/example/rythmgame/GameCreator.java
@@ -0,0 +1,14 @@
+package com.example.rythmgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameCreator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_creator);
+    }
+}
